e50a6e3d5a8bca3f13057ce5666f4a095904ce8f
Gerald Versluis <gerald@verslu.is>
2017-01-21 13:17:20 +0100

    Initial commit
    Basic app setup, functionality and UI

---
+++ TestCloudDemo/TestCloudDemoPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace TestCloudDemo
+{
+    public partial class TestCloudDemoPage : ContentPage
+    {
+        public TestCloudDemoPage ()
+        {
+            InitializeComponent ();
+        }
+
+        void Succeed_Clicked (object sender, System.EventArgs e)
+        {
+            ResultLabel.Text = "Hooray";
+            ResultLabel.TextColor = Color.Green;
+        }
+
+        void Failed_Clicked (object sender, System.EventArgs e)
+        {
+            ResultLabel.Text = "Whoops, that is embarrassing";
+            ResultLabel.TextColor = Color.Green;
+        }
+    }
+}
